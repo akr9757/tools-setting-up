029b27d068ea80af4e50ec33a5206e7664ea30db
akr9757 <anilkumareddy9757@gmail.com>
2024-08-31 18:02:17 +0530

    setting up elk

--- elk/ansible.sh
+++ elk/ansible.sh
@@ -5,4 +5,4 @@
 ssh_username=$(aws ssm get-parameter --name "ssh.username" --query "Parameter.Value" --output text)
 ssh_password=$(aws ssm get-parameter --name "ssh.password" --query "Parameter.Value" --output text)
 
-ansible-playbook -i 172.31.92.76, prometheus.yml -e ansible_user=${ssh_username} -e ansible_password=${ssh_password}
+ansible-playbook -i 172.31.28.117, elk.yml -e ansible_user=${ssh_username} -e ansible_password=${ssh_password}
